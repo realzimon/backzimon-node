a1727ce425018e92eac216d993f8c0fde6c0d366
DZDomi <dzdomi@gmail.com>
2016-12-27 14:55:34 +0100

    Init

---
+++ routes/quotes.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+var quotes = [
+  '#DanielVCS',
+  'Sorry not sorry',
+  'Daniel machts, keine Anfechtung möglich'
+];
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.json(quotes);
+});
+
+router.get('/random', function(req, res){
+  res.json(quotes[Math.floor(Math.random() * quotes.length)])
+});
+
+module.exports = router;
