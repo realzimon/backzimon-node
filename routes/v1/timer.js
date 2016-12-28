64d80346abc1cace3babba1c1439dae807c77eb2
Dominik De Zordo <dzdomi@gmail.com>
2016-12-28 22:55:56 +0100

    Added timer for the who is doing it command

--- routes/v1/timer.js
+++ routes/v1/timer.js
@@ -1,30 +1,17 @@
 var io = require('socket.io')(4001);
 
 //Seconds when the zivis should change
-const MAX_TIME = 5;
+const MAX_TIME = 120;
 //Every ten minutes update the client 600000
 var remaining = MAX_TIME;
 setInterval(countDown, 1000);
 
-io.on('connection', function (socket) {
-  io.emit('timer', {
-    remaining: remaining
-  });
-
-  socket.on('remaining', function (from, msg) {
-    io.emit('timer', {
-      remaining: remaining
-    });
-  });
-
-});
-
 function countDown(){
   remaining--;
+  io.sockets.emit('timer', {
+    remaining: remaining
+  });
   if(remaining === 0){
-    io.sockets.emit('timer', {
-      refresh: true
-    });
     remaining = MAX_TIME;
   }
 }
