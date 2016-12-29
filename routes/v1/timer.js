a41d22a20fbff3d56e987b3b31cd3dd697567822
Literallie <devnull@nowak-at.net>
2016-12-29 14:54:26 +0100

    Make post timer code a little cleaner; #10

--- routes/v1/timer.js
+++ routes/v1/timer.js
@@ -14,7 +14,7 @@ shuffleAndUpdateZivis();
 
 //Set the timeout on the first start and then every day after (86400000)
 setPostInterval();
-setInterval(setPostInterval, 30000);
+setInterval(setPostInterval, 86400000);
 var lastPostZivi;
 
 function countDown() {
@@ -28,84 +28,52 @@ function countDown() {
   }
 }
 
-function shuffleAndUpdateZivis(){
+function shuffleAndUpdateZivis() {
   models.Zivi.find({}).then(function (result) {
     shuffle(result);
     result[0].first += 1;
-    for(var i = 0; i < result.length; i++){
+    for (var i = 0; i < result.length; i++) {
       result[i].order = i + 1;
       result[i].save();
     }
   });
 }
 
-function setPostInterval(){
-
+function setPostInterval() {
   var now = new Date();
-  //dont do anything on the weekends
-  if(now.getDay() == 6 || now.getDay() == 0){
+  // don't do anything on the weekends
+  if (now.getDay() == 6 || now.getDay() == 0) {
     return;
   }
 
-  var millisTill10 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 10, 0, 0, 0) - now;
-  var millisTill14 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 14, 0, 0, 0) - now;
-  var millisTill1115 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 11, 15, 0, 0) - now;
-  var millisTill1515 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 15, 15, 0, 0) - now;
-  var millisTill1130 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 11, 30, 0, 0) - now;
-  var millisTill1530 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 15, 30, 0, 0) - now;
-
-  //TODO: Remove
-  var test = new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes(), now.getSeconds() + 5, 0) - now;
-
-  //If the milliseconds are lower then 0 the time already passed, we have to set the intervall for the next day
-  if(millisTill10 < 0){
-    millisTill10 = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1, 10, 0, 0, 0) - now;
-  }
-  if(millisTill14 < 0){
-    millisTill14 = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1, 14, 0, 0, 0) - now;
-  }
-  if(millisTill1115 < 0){
-    millisTill1115 = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1, 11, 15, 0, 0) - now;
-  }
-  if(millisTill1515 < 0){
-    millisTill1515 = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1, 15, 15, 0, 0) - now;
-  }
-  if(millisTill1130 < 0){
-    millisTill1130 = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1, 11, 30, 0, 0) - now;
-  }
-  if(millisTill1530 < 0){
-    millisTill1530 = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1, 15, 30, 0, 0) - now;
+  function millisUntilTime(hour, minute) {
+    var millis = new Date(now.getFullYear(), now.getMonth(), now.getDate(), hour, minute, 0, 0) - now;
+    if (millis < 0) { //already past this time today, set for tomorrow
+      millis = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1, hour, minute, 0, 0) - now;
+    }
+    return millis;
   }
 
-  setTimeout(changeStateToPrep, millisTill10);
-  setTimeout(changeStateToPrep, millisTill14);
-
-  setTimeout(changeStateToReminder, millisTill1115);
-  setTimeout(changeStateToReminder, millisTill1515);
-
-  setTimeout(changeStateToIdle, millisTill1130);
-  setTimeout(changeStateToIdle, millisTill1530);
-
-  //TODO: Remove
-  setTimeout(changeStateToPrep, test);
-  //5 seconds later
-  setTimeout(changeStateToReminder, test + 10000);
-  setTimeout(changeStateToIdle, test + 20000);
-
+  setTimeout(changeStateToPrep, millisUntilTime(10, 45));
+  setTimeout(changeStateToPrep, millisUntilTime(14, 45));
+  setTimeout(changeStateToReminder, millisUntilTime(11, 10));
+  setTimeout(changeStateToReminder, millisUntilTime(15, 10));
+  setTimeout(changeStateToIdle, millisUntilTime(11, 30));
+  setTimeout(changeStateToIdle, millisUntilTime(15, 30));
 }
 
-function changeStateToPrep(){
-  models.Post.findOne({}).then(function(post){
+function changeStateToPrep() {
+  models.Post.findOne({}).then(function (post) {
     //This should not happen, if everybody watches their shit
     //TODO: Uncomment
     /*if(post.state !== 'IDLE'){
-      return console.log('Somebody missed the accept the post offer');
-    }*/
+     return console.log('Somebody missed the accept the post offer');
+     }*/
     models.Zivi.find({
       name: {
         $ne: post.zivi ? post.zivi.name : ''
       }
-    }).then(function(zivis){
+    }).then(function (zivis) {
       shuffle(zivis);
       var chosenOne = zivis[0];
 
@@ -113,8 +81,8 @@ function changeStateToPrep(){
       post.timestamp = new Date();
       post.zivi = chosenOne;
 
-      post.save(function(err, result){
-        if(err){
+      post.save(function (err, result) {
+        if (err) {
           return console.log('Something wrent wrong:', err);
         }
         sendPostUpdate();
@@ -124,22 +92,22 @@ function changeStateToPrep(){
   });
 }
 
-function changeStateToReminder(){
-  models.Post.findOne({}).then(function(post){
-    if(post.state !== STATES.ACTION && post.state !== STATES.PREPERATION){
+function changeStateToReminder() {
+  models.Post.findOne({}).then(function (post) {
+    if (post.state !== STATES.ACTION && post.state !== STATES.PREPERATION) {
       return console.log('Something went terribly wrong');
     }
     //User has accepted the request and is now back (probably) or forgot to accept the offer
-    models.Zivi.findOne({name: post.zivi.name}).then(function(zivi){
+    models.Zivi.findOne({name: post.zivi.name}).then(function (zivi) {
       zivi.post_count += 1;
-      zivi.save(function(err){
-        if(err){
+      zivi.save(function (err) {
+        if (err) {
           return console.log('Something went wrong on user update', err);
         }
         post.state = STATES.REMINDER;
         post.timestamp = new Date();
-        post.save(function(err){
-          if(err){
+        post.save(function (err) {
+          if (err) {
             return console.log('Something went wrong on post update', err);
           }
           sendPostUpdate();
@@ -150,15 +118,15 @@ function changeStateToReminder(){
   });
 }
 
-function changeStateToIdle(){
-  models.Post.findOne({}).then(function(post){
-    if(post.state !== STATES.REMINDER){
+function changeStateToIdle() {
+  models.Post.findOne({}).then(function (post) {
+    if (post.state !== STATES.REMINDER) {
       return console.log('Something went terribly wrong');
     }
     post.state = STATES.IDLE;
     post.timestamp = new Date();
-    post.save(function(err){
-      if(err){
+    post.save(function (err) {
+      if (err) {
         return console.log('Something went wrong on post update', err);
       }
       sendPostUpdate();
@@ -166,7 +134,7 @@ function changeStateToIdle(){
   });
 }
 
-function sendPostUpdate(){
+function sendPostUpdate() {
   io.sockets.emit('post', {
     update: 'state'
   });
