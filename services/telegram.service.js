67cd9da78149df1f12dbff01f0c72eff85b429b4
Dominik De Zordo <dzdomi@gmail.com>
2017-01-02 15:04:07 +0100

    Improve bot so far that it caan now be considered working

--- services/telegram.service.js
+++ services/telegram.service.js
@@ -1,26 +1,31 @@
+var PostService = require('./../services/post.service');
+var ZiviService = require('./../services/zivi.service');
+var STATES = require('../config/states');
+
 var TelegramBot = require('node-telegram-bot-api');
 var bot = new TelegramBot(' -- insert api key --', {
   polling: true
 });
 
-var PostService = require('./../services/post.service');
-var ZiviService = require('./../services/zivi.service');
-
 var TelegramService = {};
 
-bot.onText(/\/postler/, function(msg, match){
-  PostService.findCurrentState(function(post){
-    if(post.state === STATES.IDLE){
-      return bot.sendMessage(msg.chat.id, 'Currently there is no postler selected (state: IDLE)');
-    }
-    return bot.sendMessage(msg.chat.id, 'The selected postler is: ' + post.zivi.name);
-  });
+bot.onText(/\/start/, function(msg, match){
+  return bot.sendMessage(msg.chat.id, 'To initialise this bot with your profile write: /init <Your first name> (Case sensitive)\n' +
+                                      'For help type: /help');
+});
+
+bot.onText(/\/help/, function(msg, match){
+  return bot.sendMessage(msg.chat.id, 'Following commands are available:\n' +
+                                      '/init <First Name> - Initialises this bot to work with your account\n' +
+                                      '/postler - Gets the current selected postler zivi for the delivery\n' +
+                                      '/accept - Accepts the post request from the bot and increments the post counter by 1\n' +
+                                      '/next - Selects the next zivi (chosen randomly and fairly)\n');
 });
 
 bot.onText(/\/init (.+)/, function(msg, match){
   ZiviService.findOneByName(match[1] ,function(zivi){
     if(!zivi){
-      return bot.sendMessage(msg.chat.id, 'No zivi found for this name (Case Sensitive)');
+      return bot.sendMessage(msg.chat.id, 'No zivi found for this name (Case sensitive)');
     }
     zivi.chat = msg.chat.id;
     ZiviService.saveZivi(zivi, function(err){
@@ -30,16 +35,33 @@ bot.onText(/\/init (.+)/, function(msg, match){
       bot.sendMessage(msg.chat.id, 'Updated your status, you will now receive post updates');
     });
   });
+});
 
+bot.onText(/\/postler/, function(msg, match){
+  if(!isAccountInitialised(msg)){
+    return bot.sendMessage(msg.chat.id, 'This account is not initialized with this bot, for more info type /help');
+  }
+  PostService.findCurrentState(function(post){
+    if(post.state === STATES.IDLE){
+      return bot.sendMessage(msg.chat.id, 'Currently there is no postler selected (state: IDLE)');
+    }
+    if(!post.zivi){
+      return bot.sendMessage(msg.chat.id, 'Currently there is no postler selected');
+    }
+    return bot.sendMessage(msg.chat.id, 'The selected postler is: ' + post.zivi.name);
+  });
 });
 
 bot.onText(/\/accept/, function(msg, match){
+  if(!isAccountInitialised(msg)){
+    return bot.sendMessage(msg.chat.id, 'This account is not initialized with this bot, for more info type /help');
+  }
   PostService.findCurrentState(function(post){
     if(post.state !== STATES.PREPERATION){
-      //return bot.sendMessage(msg.chat.id, 'Post is not in preparing state');
+      return bot.sendMessage(msg.chat.id, 'Post is not in preparing state');
     }
-    if(post.zivi.chat !== msg.chat.id){
-      //return bot.sendMessage(msg.chat.id, 'You are not the selected postler');
+    if(!post.zivi || post.zivi.chat !== msg.chat.id){
+      return bot.sendMessage(msg.chat.id, 'You are not the selected postler');
     }
     PostService.acceptPost(function(){
       bot.sendMessage(msg.chat.id, 'Post accepted');
@@ -50,11 +72,14 @@ bot.onText(/\/accept/, function(msg, match){
 
 bot.onText(/\/next/, function(msg, match){
 
+  if(!isAccountInitialised(msg)){
+    return bot.sendMessage(msg.chat.id, 'This account is not initialized with this bot, for more info type /help');
+  }
   PostService.findCurrentState(function(post){
     if(post.state !== STATES.PREPERATION){
       return bot.sendMessage(msg.chat.id, 'Post is not in preparing state');
     }
-    if(post.zivi.chat !== msg.chat.id){
+    if(!post.zivi || post.zivi.chat !== msg.chat.id){
       return bot.sendMessage(msg.chat.id, 'You are not the selected postler');
     }
     PostService.nextZivi(function(err, zivi){
@@ -71,5 +96,17 @@ TelegramService.sendZiviUpdateToUser = function(zivi, message){
   }
   bot.sendMessage(zivi.chat, message);
 };
+
+function isAccountInitialised(msg){
+  var chatId = msg.chat.id;
+  ZiviService.findAll(function(zivis){
+    zivis.forEach(function(zivi){
+      if(zivi.chat === chatId){
+        return true;
+      }
+    });
+    return false;
+  });
+}
 
 module.exports = TelegramService;
