edbb370e94c1a4672fcbb4916924595c17065140
Literallie <devnull@nowak-at.net>
2017-01-03 13:53:16 +0100

    Add dummy Telegram service for when there's no API key

---
+++ services/telegram-dummy.service.js
@@ -0,0 +1,9 @@
+/**
+ * No-op dummy implementation of the Telegram service, so that it can still be called even if
+ * there is no API key. Do not require this directly.
+ */
+var TelegramService = {};
+
+TelegramService.sendZiviUpdateToUser = function () {
+
+};
--- services/telegram.service.js
+++ services/telegram.service.js
@@ -2,40 +2,41 @@ var PostService = require('./post.service');
 var ZiviService = require('./zivi.service');
 var ConfigService = require('./config.service');
 var STATES = require('../models/states');
-var TelegramBot = require('node-telegram-bot-api');
-
 const apiKey = ConfigService.getTelegramApiKey();
-if(!apiKey) {
-  console.error('Starting without a Telegram API key is not supported yet. Please set yours in ./config/local-config.json');
-  process.exit(1);
+var TelegramService = require('./telegram-dummy.service');
+
+if (!apiKey || process.env.zimonTest) {
+  console.warn('Using dummy Telegram service, i.e. ignoring all calls.');
+  console.warn('Set an API key in ./config/local-config.json to change this.');
+  return;
 }
+
+var TelegramBot = require('node-telegram-bot-api');
 var bot = new TelegramBot(apiKey, {
   polling: true
 });
 
-var TelegramService = {};
-
-bot.onText(/\/start/, function(msg, match){
+bot.onText(/\/start/, function (msg, match) {
   return bot.sendMessage(msg.chat.id, 'To initialise this bot with your profile write: /init <Your first name> (Case sensitive)\n' +
-                                      'For help type: /help');
+    'For help type: /help');
 });
 
-bot.onText(/\/help/, function(msg, match){
+bot.onText(/\/help/, function (msg, match) {
   return bot.sendMessage(msg.chat.id, 'Following commands are available:\n' +
-                                      '/init <First Name> - Initialises this bot to work with your account\n' +
-                                      '/postler - Gets the current selected postler zivi for the delivery\n' +
-                                      '/accept - Accepts the post request from the bot and increments the post counter by 1\n' +
-                                      '/next - Selects the next zivi (chosen randomly and fairly)\n');
+    '/init <First Name> - Initialises this bot to work with your account\n' +
+    '/postler - Gets the current selected postler zivi for the delivery\n' +
+    '/accept - Accepts the post request from the bot and increments the post counter by 1\n' +
+    '/next - Selects the next zivi (chosen randomly and fairly)\n');
 });
 
-bot.onText(/\/init (.+)/, function(msg, match){
-  ZiviService.findOneByName(match[1] ,function(zivi){
-    if(!zivi){
+bot.onText(/\/init (.+)/, function (msg, match) {
+  ZiviService.findOneByName(match[1], function (zivi) {
+    if (!zivi) {
       return bot.sendMessage(msg.chat.id, 'No zivi found for this name (Case sensitive)');
     }
     zivi.chat = msg.chat.id;
-    ZiviService.saveZivi(zivi, function(err){
-      if(err){
+    ZiviService.saveZivi(zivi, function (err) {
+      if (err) {
         return bot.sendMessage(msg.chat.id, 'Something went wrong while saving');
       }
       bot.sendMessage(msg.chat.id, 'Updated your status, you will now receive post updates');
@@ -43,9 +44,9 @@ bot.onText(/\/init (.+)/, function(msg, match){
   });
 });
 
-bot.onText(/\/postler/, function(msg, match){
+bot.onText(/\/postler/, function (msg, match) {
 
-  isAccountInitialised(msg, function(init) {
+  isAccountInitialised(msg, function (init) {
     if (!init) {
       return bot.sendMessage(msg.chat.id, 'This account is not initialized with this bot, for more info type /help');
     }
@@ -62,8 +63,8 @@ bot.onText(/\/postler/, function(msg, match){
 
 });
 
-bot.onText(/\/accept/, function(msg, match){
-  isAccountInitialised(msg, function(init) {
+bot.onText(/\/accept/, function (msg, match) {
+  isAccountInitialised(msg, function (init) {
     if (!init) {
       return bot.sendMessage(msg.chat.id, 'This account is not initialized with this bot, for more info type /help');
     }
@@ -82,20 +83,20 @@ bot.onText(/\/accept/, function(msg, match){
   });
 });
 
-bot.onText(/\/next/, function(msg, match){
+bot.onText(/\/next/, function (msg, match) {
 
-  isAccountInitialised(msg, function(init){
-    if(!init){
+  isAccountInitialised(msg, function (init) {
+    if (!init) {
       return bot.sendMessage(msg.chat.id, 'This account is not initialized with this bot, for more info type /help');
     }
-    PostService.findCurrentState(function(post){
-      if(post.state !== STATES.PREPARATION){
+    PostService.findCurrentState(function (post) {
+      if (post.state !== STATES.PREPARATION) {
         return bot.sendMessage(msg.chat.id, 'Post is not in preparing state');
       }
-      if(!post.zivi || post.zivi.chat !== msg.chat.id){
+      if (!post.zivi || post.zivi.chat !== msg.chat.id) {
         return bot.sendMessage(msg.chat.id, 'You are not the selected postler');
       }
-      PostService.nextZivi(function(err, zivi){
+      PostService.nextZivi(function (err, zivi) {
         bot.sendMessage(msg.chat.id, 'Selected another zivi');
         PostService.pushPostState();
         TelegramService.sendZiviUpdateToUser(zivi, 'You are the selected postler');
@@ -104,19 +105,19 @@ bot.onText(/\/next/, function(msg, match){
   });
 });
 
-TelegramService.sendZiviUpdateToUser = function(zivi, message){
-  if(zivi.chat === -1){
+TelegramService.sendZiviUpdateToUser = function (zivi, message) {
+  if (zivi.chat === -1) {
     return console.log('Zivi ' + zivi.name + ' does not have a valid chat id');
   }
   bot.sendMessage(zivi.chat, message);
 };
 
-function isAccountInitialised(msg, callback){
+function isAccountInitialised(msg, callback) {
   var chatId = msg.chat.id;
-  ZiviService.findAll(function(zivis){
+  ZiviService.findAll(function (zivis) {
     var init = false;
-    zivis.forEach(function(zivi){
-      if(zivi.chat === chatId){
+    zivis.forEach(function (zivi) {
+      if (zivi.chat === chatId) {
         init = true;
       }
     });
