0885ba801766da7ef3656fd06d90b643821cc5cb
Literallie <git@l1t.li>
2017-06-02 14:38:46 +0200

    Only log failing requests

--- app.js
+++ app.js
@@ -27,7 +27,11 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(logger('dev'));
+app.use(logger('dev', {
+  skip: function (req, res) {
+    return res.statusCode < 400
+  }
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
