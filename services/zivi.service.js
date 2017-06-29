e962659a3af326f1bd4106aeb1693c96ca7e5356
Literallie <git@l1t.li>
2017-06-29 17:02:33 +0200

    Add zivi update by name to zivi.service

--- services/zivi.service.js
+++ services/zivi.service.js
@@ -22,17 +22,32 @@ ZiviService.findAllBut = function (ziviExcluded, callback) {
   }
 };
 
-ZiviService.saveZivi = function(zivi, callback){
+ZiviService.saveZivi = function (zivi, callback) {
   zivi.save(callback);
 };
 
 ZiviService.deleteZivi = function (zivi, callback) {
   models.DeletedZivi.create(zivi, function (err) {
-    if(err) {
+    if (err) {
       callback && callback(err);
     } else {
       zivi.remove(callback);
     }
+  })
+};
+
+ZiviService.createZivi = function (obj, callback) {
+  models.Zivi.create(obj, callback);
+};
+
+ZiviService.updateZiviByName = function (name, spec, callback) {
+  ZiviService.findOneByName(name, function (zivi) {
+    for(var propName in obj) {
+      if(obj.hasOwnProperty(propName) && propName !== '_id') {
+        zivi[propName] = obj[propName];
+      }
+    }
+    ZiviService.saveZivi(zivi, callback);
   })
 };
 
