fa1a4a218170b9757445eb594221d6d2cb349019
Literallie <git@l1t.li>
2017-06-30 13:14:56 +0200

    Add create support to zivis endpoint

--- routes/v1/zivis.js
+++ routes/v1/zivis.js
@@ -5,7 +5,7 @@ var models = require('../../models/index');
 var ZiviService = require('../../services/zivi.service');
 
 router.get('/', function (req, res) {
-  models.Zivi.find({}).sort('order').exec(function(err, response){
+  models.Zivi.find({}).sort('order').exec(function (err, response) {
     return res.json({
       zivicount: response.length,
       zivis: response
@@ -14,22 +14,22 @@ router.get('/', function (req, res) {
 });
 
 router.get('/random', function (req, res) {
-  models.Zivi.find({}).then(function(response){
+  models.Zivi.find({}).then(function (response) {
     return res.json(response[Math.floor(Math.random() * response.length)])
   });
 });
 
 router.post('/delete', function (req, res) {
   var name = req.body.name;
-  if(!name) {
+  if (!name) {
     return res.status(400).json({error: "No name given"});
   }
   var zivi = ZiviService.findOneByName(name, function (err, zivi) {
-    if(err) {
+    if (err) {
       return res.status(500).json({error: err});
     }
     ZiviService.deleteZivi(zivi, function (err) {
-      if(err) {
+      if (err) {
         return res.status(500).json({error: err});
       } else {
         return res.status(200).json({error: false});
@@ -41,11 +41,25 @@ router.post('/delete', function (req, res) {
 router.post('/update', function (req, res) {
   var name = req.body.name;
   var spec = req.body.spec;
-  if(!name || typeof name !== "string" || !spec || typeof spec !== "object") {
+  if (!name || typeof name !== "string" || !spec || typeof spec !== "object") {
     return res.status(400).json({error: "body needs name (String) and spec (object) properties"});
   }
   ZiviService.updateZiviByName(name, spec, function (err, zivi) {
-    if(err) {
+    if (err) {
+      return res.status(500).json({error: err});
+    } else {
+      return res.status(200).json({zivi: zivi});
+    }
+  });
+});
+
+router.post('/create', function (req, res) {
+  var spec = req.body.spec;
+  if (!spec || typeof spec !== "object") {
+    return res.status(400).json({error: "body needs spec (object) property"});
+  }
+  ZiviService.createZivi(spec, function (err, zivi) {
+    if (err) {
       return res.status(500).json({error: err});
     } else {
       return res.status(200).json({zivi: zivi});
