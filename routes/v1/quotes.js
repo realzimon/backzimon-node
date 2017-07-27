6a4f19f1dfaf4d0d3ed55c2b2ce6370353a0bc8b
Literallie <git@l1t.li>
2017-07-27 11:12:32 +0200

    Add API endpoint to update existing quotes

--- routes/v1/quotes.js
+++ routes/v1/quotes.js
@@ -4,34 +4,33 @@ var router = express.Router();
 var models = require('../../models/index');
 
 router.get('/', function (req, res) {
-  models.Quote.find({}).then(function(response){
-    return res.json(response);
+  models.Quote.find({}).then(function (quotes) {
+    return res.json({count: quotes.length, quotes: quotes});
   });
 });
 
 router.get('/random', function (req, res) {
-
-  models.Quote.find({}).then(function(response){
+  models.Quote.find({}).then(function (response) {
     return res.json({
       quote: response[Math.floor(Math.random() * response.length)]
     })
   });
 });
 
-router.post('/', function(req, res){
+router.post('/', function (req, res) {
   var text = req.body.text;
-  if(!text){
+  if (!text) {
     return res.status(400).json({
       err: 'No text given'
     })
   }
-  models.Quote.findOne({text: text}, function(err, result){
-    if(err){
+  models.Quote.findOne({text: text}, function (err, result) {
+    if (err) {
       return res.status(500).json({
         err: err
       })
     }
-    if(result){
+    if (result) {
       return res.status(200).json();
     }
     var quote = new models.Quote({
@@ -39,13 +38,40 @@ router.post('/', function(req, res){
       count: 0
     });
 
-    quote.save(function(err){
-      if(err){
+    quote.save(function (err) {
+      if (err) {
         return res.status(500).json({
           err: err
         })
       }
       return res.status(201).json();
+    });
+  });
+
+});
+
+router.post('/update', function (req, res) {
+  var originalText = req.body && req.body.originalText;
+  var newText = req.body && req.body.newText;
+  if (!originalText || !newText) {
+    return res.status(400).json({
+      err: 'Need both originalText and newText.'
+    })
+  }
+  models.Quote.findOne({text: originalText}, function (err, quote) {
+    if (err || !quote) {
+      return res.status(500).json({
+        err: err || 'unknown error'
+      })
+    }
+    console.log(' --- changing quote text from "' + originalText + '" to "' + newText + '"');
+    quote.text = newText;
+    quote.save(function (err, savedQuote) {
+      if (err) {
+        return res.status(500).json({err: err});
+      } else {
+        return res.status(200).json({newText: savedQuote.text});
+      }
     });
   });
 
